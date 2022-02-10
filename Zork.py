4f29a75934e242920471bf807d57b2baa98f2e0e
TaxifahrerAli <96825492+TaxifahrerAli@users.noreply.github.com>
2022-02-10 18:11:16 +0100

    Zork Iteration 6 fixed

--- Zork.py
+++ Zork.py
@@ -16,22 +16,24 @@ def show_invetory():
         "Du hast den Schatz." if not state["treasureAvail"] else "")
 
 
+def speichern(mem, state):
+    if mem == "S)":
+        with open('save.json', 'w') as fp:
+            fp.write(json.dumps(state))
+        return {**state, "hp": 0}
+    else:
+        return state 
+
+
 def spiel_menue(state):
     memory = input("Spielmenü:\n1) Neues Spiel\n2) Spiel laden\n-- ")
-    with open("save.json","r") as fp:
-        newstate = json.loads("%s" %fp.read())
-    if "" != newstate:
-        if memory == "1)":
-            print("Spiel wird gestartet...") #Zusatzidee
-            position = "Eingang"
-            return {**state, "position" : "Eingang"}
-        elif memory == "2)":
-            return newstate
-    else:
-        if memory == "1)":
-            print("Spiel wird gestartet...") #Zusatzidee
-            position = "Eingang"
-            return {**state, "position" : "Eingang"}
+    if memory == "1)":
+        print("Spiel wird gestartet...") #Zusatzidee
+        return {**state, "position" : "Eingang"}
+    elif memory == "2)":
+        with open("save.json","r") as fp:
+            newstate = json.loads("%s" %fp.read())
+        return newstate
     return state
 
 
@@ -45,10 +47,8 @@ def room_eingang(state):
             return {**state, "position" : "Schatzkammer"}
         elif memory == "2)":
             return {**state, "position" : "Handler"}
-        elif memory == "S)":
-            with open('save.json', 'w') as fp:
-                fp.write(json.dumps(state))
-            return {**state, "hp": 0}
+        else:
+            return speichern(memory, state)
     elif not state["treasureAvail"]:
         memory = input("1) Schatzkammer\n2) Händler\n3) Beenden\nS) Speichern und Beenden\n-- ")
         if memory == "1)":
@@ -57,10 +57,8 @@ def room_eingang(state):
             return {**state, "position" : "Handler"}
         elif memory == "3)":
             return {**state, "hp": 0}
-        elif memory == "S)":
-            with open('save.json', 'w') as fp:
-                fp.write(json.dumps(state))
-            return {**state, "hp": 0}
+        else:
+            return speichern(memory, state)
     return state
 
 def room_schatzk(state):
@@ -70,7 +68,7 @@ def room_schatzk(state):
     show_invetory()
 
     if state["dragonAlive"]:
-        memory = input("1) Drachen bekämpfen\n2) Zurück\n-- ")
+        memory = input("1) Drachen bekämpfen\n2) Zurück\nS) Speichern und Beenden\n-- ")
         if memory == "1)":
             randomint = randint(1, 6)
             if ((randomint < 4 and not state["swordAvail"])
@@ -81,16 +79,22 @@ def room_schatzk(state):
                 return {**state, "hp": state["hp"] - 1}
         elif memory == "2)":
             return {**state, "position" : "Eingang"}
+        else:
+            return speichern(memory, state)
     elif not state["dragonAlive"] and state["treasureAvail"]:
-        memory = input("1) Schatz aufheben\n2) Zurück\n-- ")
+        memory = input("1) Schatz aufheben\n2) Zurück\nS) Speichern und Beenden\n-- ")
         if memory == "1)":
             print("Schatz aufgehoben!")
             return {**state, "treasureAvail": False}
         elif memory == "2)":
             return {**state, "position" : "Eingang"}
+        else:
+            return speichern(memory, state)
     elif not state["dragonAlive"] and not state["treasureAvail"]:
-        if input("1) Zurück\n-- ") == "1)":
+        if input("1) Zurück\nS) Speichern und Beenden\n-- ") == "1)":
             return {**state, "position" : "Eingang"}
+        else:
+            return speichern(memory, state)
     return state
 
 
@@ -101,15 +105,17 @@ def room_handler(state):
     show_invetory()
 
     if state["swordAvail"]:
-        memory = input("1) Schwert Kaufen\n2) Zurück\n-- ")
+        memory = input("1) Schwert Kaufen\n2) Zurück\nS) Speichern und Beenden\n-- ")
         if memory == "1)":
             return {**state, "swordAvail": False, "hp": state["hp"] - 1}
         elif memory == "2)":
             return {**state, "position" : "Eingang"}
     else:
-        memory = input("1) Zurück\n-- ")
+        memory = input("1) Zurück\nS) Speichern und Beenden\n-- ")
         if memory == "1)":
             return {**state, "position" : "Eingang"}
+        else:
+            return speichern(memory, state)
     return state
 
 
