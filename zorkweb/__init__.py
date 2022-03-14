b2e3a69bbc3978f03582d7237d8a046a2033dce5
TaxifahrerAli <96825492+TaxifahrerAli@users.noreply.github.com>
2022-03-14 16:40:25 +0100

    Zork Web: Profil, Login Seiten fertig

--- zorkweb/__init__.py
+++ zorkweb/__init__.py
@@ -1,6 +1,6 @@
 import json
 import pg8000
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, session, redirect, render_template
 from . import room_eingang
 from . import room_schatzkammer
 from . import room_handler
@@ -52,6 +52,7 @@ def save_game(state):
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'sldfnalsdnf5'
 
 
 @app.route("/zork", methods=["POST"])
@@ -77,10 +78,10 @@ def show_game():
     return render_template("template.html", beschreibung=beschreibung, optionen=optionen, state=state)
 
 @app.route("/users", methods=["GET"])
-def userliste():
+def userliste_rendern():
     conn = pg8000.connect(user="zork", password="zork", database="zork")
     cursor = conn.cursor()
-    cursor.execute("select username from users")
+    cursor.execute("select id, username from users")
     rows = cursor.fetchall()
     conn.commit()
     conn.close()
@@ -90,14 +91,9 @@ def userliste():
 def user_löschen():
     conn = pg8000.connect(user="zork", password="zork", database="zork")
     cursor = conn.cursor()
-    
+
     userLöschen = request.form.get('choice')
-    name = ""
-    for i in userLöschen:
-        if i != "[" and i != "]" and i != "'":
-            print(i)
-            name += i
-    cursor.execute("delete from users where username = %s", [name])
+    cursor.execute("delete from users where id = %s", [userLöschen])
 
     conn.commit()
     conn.close()
@@ -105,7 +101,7 @@ def user_löschen():
 
 
 @app.route("/createuser", methods=["GET"])
-def userlists():
+def form_rendern():
     return render_template("createUser.html")
 
 @app.route("/createuser", methods=["POST"])
@@ -119,3 +115,35 @@ def datenbank_bearbeiten():
     conn.commit()
     conn.close()
     return redirect("/users", code=302)
+
+@app.route("/login", methods=["GET"])
+def login():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def pruefung():
+    conn = pg8000.connect(user="zork", password="zork", database="zork")
+    cursor = conn.cursor()
+    username = request.form.get('username')
+    password = request.form.get('password')
+    print(username)
+    cursor.execute("select id from users where username = %s and password = %s", [username, password])
+    user = cursor.fetchall()
+    if user:
+        session["userid"] = user[0][0]
+        print(user)
+        return redirect("/profil", code=302)
+    else:
+        return redirect("/login", code=302)
+
+@app.route("/profil", methods=["GET"])
+def sofjds():
+    conn = pg8000.connect(user="zork", password="zork", database="zork")
+    cursor = conn.cursor()
+    id = session["userid"]
+    if id:
+        cursor.execute("select username from users where id = %s", [id])
+        username = cursor.fetchall()
+        return render_template("profil.html", username=username[0][0])
+    else:
+        return render_template("profil.html", username="none")
